e12f4c3facdeffc50b45724d3c19834da3fc5237
matze328 <152269645+matze328@users.noreply.github.com>
2024-05-06 09:07:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const todoSequelize = require("./src/database/setup/database");
 const app = require("./src/server");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { AppRouter } = require("./src/routes");
 
 // Zugriff auf Umgebungsvariablen
 // const { PORT } = process.env;
 const PORT = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.use(cors());
 
 todoSequelize
   .sync()
@@ -13,8 +21,9 @@ todoSequelize
   .catch((e) => {
     console.log(e);
   });
-
+  app.use("/v1", AppRouter);
 // App hört im folgenden auf den Port, welcher über die Umgebungsvariable definiert ist
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
